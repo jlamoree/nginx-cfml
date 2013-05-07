ec6b12222ecbb631a59d593f887d2b45f34155e3
Joseph Lamoree <joseph@lamoree.com>
2013-05-07 12:46:47 -0700

    Added better checking to see if the process is running.

--- bin/nginx.sh
+++ bin/nginx.sh
@@ -1,39 +1,49 @@
-#!/bin/bash
+#!/bin/bash 
 
 NGINX_PROJECT=
 NGINX_CONF=${1:-_}
 NGINX_BIN=/usr/sbin/nginx
 NGINX_PREFIX=
+NGINX_PID=
 
 function usage {
-	echo "Usage: `basename $0` configfile"
+  echo "Usage: `basename $0` configfile"
 }
 
 function error {
-	echo "Error: $@"
-	usage
-	echo
-	exit 1
+  echo "Error: $@"
+  usage
+  echo
+  exit 1
 }
 
 if [ "_$NGINX_PROJECT" == "_" ]; then
-	NGINX_PROJECT=$(cd `dirname $0`; cd ..; pwd -P)
+  NGINX_PROJECT=$(cd `dirname $0`; cd ..; pwd -P)
 fi
 
-if [ "_$NGINX_CONF" == "_" ]; then
-	error "A configuration file required"
+if [ "$NGINX_CONF" == "_" -o ! -f "$NGINX_CONF" ]; then
+  error "A configuration file required"
 fi
 NGINX_CONF="$NGINX_PROJECT/$NGINX_CONF"
 
 if [ ! -x "$NGINX_BIN" ]; then
-	error "Nginx executable is not valid"
+  error "Nginx executable is not valid"
 fi
 
 if [ "_$NGINX_PREFIX" == "_" ]; then
-	NGINX_PREFIX="$NGINX_PROJECT/common/"
+  NGINX_PREFIX="$NGINX_PROJECT/common/"
 fi
 
-if [ -f "$NGINX_PREFIX/logs/nginx.pid" ]; then
-	sudo $NGINX_BIN -p $NGINX_PREFIX -c $NGINX_CONF -s stop
+sudo $NGINX_BIN -p $NGINX_PREFIX -c $NGINX_CONF -t
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+if [ -f "${NGINX_PREFIX}logs/nginx.pid" ]; then
+  NGINX_PID=`cat "${NGINX_PREFIX}logs/nginx.pid"`
+  sudo kill -s 0 $NGINX_PID 2>&1 > /dev/null
+  if [ $? == 0 ]; then
+    sudo $NGINX_BIN -p $NGINX_PREFIX -c $NGINX_CONF -s stop
+  fi
 fi
 sudo $NGINX_BIN -p $NGINX_PREFIX -c $NGINX_CONF
